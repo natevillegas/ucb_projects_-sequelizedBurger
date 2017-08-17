4a8cde155860cdc7b59da66acfca03a38af73f25
Nathaniel Villegas <nathanielvillegas@Nathaniels-MBP.att.net>
2017-08-16 23:32:59 -0700

    test

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var burger = require("../models");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  res.render("index");
+  res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
