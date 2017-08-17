92bfe6ef658e9933aece2fee2b9a55d70a01fb8b
Nathaniel Villegas <nathanielvillegas@Nathaniels-MacBook-Pro.local>
2017-08-16 21:21:58 -0700

    fixed db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var port = process.env.PORT || 3000;
+var port = process.env.JAWSDB_URL || 3000;
 var app = express();
 var db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
@@ -21,3 +21,8 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + port);
   });
 });
+
+    // “username”: “txmywchms2cu0i59",
+    // “password”: “kyrkhwokujz2bgzj”,
+    // “database”: “burgers_db”,
+    // “host”: “dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com”,
